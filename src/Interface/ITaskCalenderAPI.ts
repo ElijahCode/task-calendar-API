fe4ca7760fe4f1a8fda726937e2cac01f067655d
Ilya <ilyastarickov1996@gmail.com>
2021-05-17 19:06:32 +0400

    style: delete comments

--- src/Interface/ITaskCalenderAPI.ts
+++ src/Interface/ITaskCalenderAPI.ts
@@ -4,11 +4,10 @@ export interface ITaskCalendar {
   tasksID: string[] | Task["id"][];
 
   createTask(newTask: Task): Promise<Task[]>;
-  read(id: Task["id"] | string): Promise<Task>; // позже уточнить возвращаемый таск, а именно то что он идет с определенным id
+  read(id: Task["id"] | string): Promise<Task>;
   update(id: Task["id"] | string, updatedTask: Partial<Task>): Promise<Task>;
   delete(id: Task["id"] | string): Promise<void>;
 
-  // сгрести в один filter через перегрузку?
   filterByDate(filtredDate: Date): Promise<Task[]>;
   filterByDescription(description: Task["description"]): Promise<Task[]>;
   filterByStatus(status: Task["status"]): Promise<Task[]>;
